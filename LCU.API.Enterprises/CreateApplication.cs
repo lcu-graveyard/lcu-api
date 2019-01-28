e560ce90a62e5258eb4be03b3843e5dfb82ffba5
Michael Gearhardt <michael.gearhardt@fathym.com>
2019-01-28 13:36:00 -0700

    Latest

--- LCU.API.Enterprises/CreateApplication.cs
+++ LCU.API.Enterprises/CreateApplication.cs
@@ -1,6 +1,5 @@
 using Fathym;
 using Fathym.API;
-using Gremlin.Net.Driver;
 using LCU.Graphs;
 using LCU.Graphs.Registry.Enterprises;
 using Microsoft.AspNetCore.Http;
@@ -9,9 +8,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -58,5 +55,54 @@ namespace LCU.API.Enterprises
 
 			return new JsonResult(response, new JsonSerializerSettings());
 		}
+
+		//public class AppGraph : ApplicationGraph
+		//{
+		//	public AppGraph(LCUGraphConfig config)
+		//		: base(config)
+		//	{ }
+
+		//	public override async Task<Application> Create(Application application)
+		//	{
+		//		return await withG(async (client, g) =>
+		//		{
+		//			var addQuery = g.AddV(AppGraphConstants.AppVertexName)
+		//				.Property("Container", application.Container)
+		//				.Property("Host", application.Host)
+		//				.Property("IsReadOnly", application.IsReadOnly)
+		//				.Property("Name", application.Name)
+		//				.Property("PathRegex", application.PathRegex)
+		//				.Property("Priority", application.Priority)
+		//				//.Property("QueryRegex", application.QueryRegex)
+		//				//.Property("UserAgentRegex", application.UserAgentRegex)
+		//				.Property("EnterprisePrimaryAPIKey", application.EnterprisePrimaryAPIKey)
+		//				.Property("Registry", application.EnterprisePrimaryAPIKey);
+
+		//			var appResults = await Submit<Application>(addQuery);
+
+		//			var appResult = appResults.FirstOrDefault();
+
+		//			var entQuery = g.V().HasLabel(AppGraphConstants.EnterpriseVertexName)
+		//				.Has("Registry", application.EnterprisePrimaryAPIKey)
+		//				.Has("PrimaryAPIKey", application.EnterprisePrimaryAPIKey);
+
+		//			var entResults = await Submit<Enterprise>(entQuery);
+
+		//			var entResult = entResults.FirstOrDefault();
+
+		//			var edgeQueries = new[] {
+		//			g.V(entResult.ID).AddE(AppGraphConstants.OwnsEdgeName).To(g.V(appResult.ID)),
+		//			g.V(entResult.ID).AddE(AppGraphConstants.ManagesEdgeName).To(g.V(appResult.ID))
+		//		};
+
+		//			foreach (var edgeQuery in edgeQueries)
+		//			{
+		//				await Submit(edgeQuery);
+		//			}
+
+		//			return appResult;
+		//		});
+		//	}
+		//}
 	}
 }
